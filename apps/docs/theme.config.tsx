a50dbf8f220c80ad758251438034880be643954b
Lionel Bertrand <lionel.bertrand@paymium.com>
2023-08-11 19:04:43 +0200

    add seo

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -6,14 +6,85 @@ import { MoonIcon, SunIcon } from 'nextra/icons';
 import { Button, useThemeContext, tw } from '@mergeui/ui';
 
 const config: DocsThemeConfig = {
-  logo: <span>mergeui</span>,
+  logo: (
+    <span className="flex flex-row items-center gap-md">
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 6.5615616 12.396698"
+        version="1.1"
+      >
+        <g transform="translate(-111.7855,-153.67251)">
+          <rect
+            fill="currentColor"
+            strokeWidth="0.162002"
+            fillOpacity="1"
+            width="10.078526"
+            height="1.5204298"
+            x="155.99068"
+            y="-113.30594"
+            transform="rotate(90)"
+          />
+          <rect
+            fill="currentColor"
+            strokeWidth="0.0609394"
+            fillOpacity="1"
+            width="1.4261072"
+            height="1.5204298"
+            x="153.67252"
+            y="-113.30762"
+            transform="rotate(90)"
+          />
+          <rect
+            fill="currentColor"
+            strokeWidth="0.136255"
+            fillOpacity="1"
+            width="7.129498"
+            height="1.5204298"
+            x="-197.55099"
+            y="-31.703402"
+            transform="rotate(-135)"
+          />
+          <rect
+            fill="currentColor"
+            strokeWidth="0.130668"
+            fillOpacity="1"
+            width="6.5568142"
+            height="1.5204298"
+            x="111.78551"
+            y="164.54877"
+          />
+        </g>
+      </svg>
+      mergeui
+    </span>
+  ),
   project: {
     link: 'https://github.com/mergeui/mergeui',
   },
-  docsRepositoryBase: 'https://github.com/mergeui/mergeui',
+  banner: {
+    text: 'Mergeui is not ready production',
+  },
+  docsRepositoryBase: 'https://github.com/mergeui/mergeui/tree/main/apps/docs',
   footer: {
     text: 'mergeui documentation',
   },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Mergeui',
+    };
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Mergeui" />
+      <meta
+        property="og:description"
+        content="Mergeui is cross platform library for react-native and react web"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  ),
   themeSwitch: {
     component: function Component() {
       const { setTheme, theme } = useThemeContext();
