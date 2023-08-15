d370db10f36085e7c953e2ef774dd618924da062
Lionel Bertrand <lionel.bertrand@paymium.com>
2023-08-15 08:37:43 +0200

    modifiate documentation page

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     './components/**/*.{js,jsx,ts,tsx,md,mdx}',
     './node_modules/@crossed/demo/lib/**/*.{js,jsx,ts,tsx,md,mdx}',
     './node_modules/@crossed/ui/lib/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './theme.config.tsx',
     // path.join(
     //   path.dirname(require.resolve('@crossed/demo')),
     //   '**/*.{js,jsx,ts,tsx,md,mdx}'
@@ -20,7 +21,21 @@ module.exports = {
   //   },
   // ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          100: '#6FCBFF',
+          200: '#0F9FFF',
+          300: '#0F79D7',
+          400: '#0056B7',
+          500: '#003C89',
+          600: '#00295E',
+          700: '#001C41',
+          800: '#00142D',
+          900: '#000E1F',
+        },
+      },
+    },
   },
   plugins: [],
 };
