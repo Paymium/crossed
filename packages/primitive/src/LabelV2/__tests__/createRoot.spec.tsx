14469f7cd89760d4c7c7226f7431eee198000e8d
aymerick.batlogg <aymerick.batlogg@paymium.com>
2024-01-29 11:49:14 +0100

    add test

---
+++ packages/primitive/src/LabelV2/__tests__/createRoot.spec.tsx
@@ -0,0 +1,38 @@
+import '@testing-library/jest-dom';
+import { createRoot } from '../createRoot';
+import { Provider } from '../context';
+import type { ReactNode } from 'react';
+import React from 'react';
+import { render, screen } from '@crossed/test';
+
+jest.mock('../context');
+const ProviderMocked = Provider as unknown as jest.Mock<ReactNode>;
+const Comp = (p: any) => <div {...p} />;
+const NewComp = createRoot(Comp);
+
+describe('createButtonMain', () => {
+  const oldUseId = React.useId;
+  beforeEach(() => {
+    React.useId = jest.fn(() => 'id');
+    ProviderMocked.mockImplementation(({ children }: any) => <>{children}</>);
+  });
+  afterEach(() => {
+    React.useId = oldUseId;
+    ProviderMocked.mockReset();
+  });
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  test('provide id', async () => {
+    const child = 'Pass child';
+    render(<NewComp>{child}</NewComp>);
+
+    expect(React.useId).toHaveBeenCalled();
+    expect(ProviderMocked).toHaveBeenCalled();
+    expect(ProviderMocked.mock.calls[0][0]).toHaveProperty('id', 'id');
+    expect(ProviderMocked.mock.calls[0][0]).toHaveProperty('children');
+    const text = await screen.getByText(child);
+    expect(text).toHaveAttribute('role', 'label');
+  });
+});
