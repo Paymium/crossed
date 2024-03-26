ce9dc0193b0dfb3fc8bf85fa36ca1df25f599628
aymerick.batlogg <aymerick.batlogg@paymium.com>
2024-03-26 16:48:30 +0100

    some fix

--- apps/documentation/src/app/(doc)/primitive/(components)/createLabel/page.tsx
+++ apps/documentation/src/app/(doc)/primitive/(components)/createLabel/page.tsx
@@ -8,7 +8,6 @@
 'use client';
 import { useTranslation } from 'react-i18next';
 import { TemplatePrimitive } from '../templatePrimitive';
-
 export default function CreateLabel() {
   const { t } = useTranslation();
   return (
@@ -20,14 +19,21 @@ export default function CreateLabel() {
       types={[]}
       anatomy={`
 import { createLabel } from "@crossed/primitive";
-const Label = createLabel({Root,Text,})
+const Label = createLabel({
+  Root,
+  Text,
+})
 <Label>
   <Label.Text />
   <Label.Input />
 </Label>`}
       example={`
 import { createLabel } from "@crossed/primitive";
-const Input = createLabel({Root,Text,});`}
+const Input = createLabel({
+  Root,
+  Text,
+});
+`}
     />
   );
 }
