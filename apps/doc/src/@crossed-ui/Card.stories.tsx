859195ae27fcfc8a543b574b037e3c834b16a3a4
aymerick <aymerick.batlogg@paymium.com>
2024-11-15 14:47:07 +0100

    add card group and test

--- apps/doc/src/@crossed-ui/Card.stories.tsx
+++ apps/doc/src/@crossed-ui/Card.stories.tsx
@@ -8,24 +8,29 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Card } from '@crossed/ui/src/display/Card';
+import { XBox } from '@crossed/ui/src/layout/XBox';
+import { Text } from '@crossed/ui/src/typography/Text';
+import { Divider } from '@crossed/ui/src/layout/Divider';
+import { Box } from '@crossed/ui/src/layout/Box';
+import { inlineStyle } from '@crossed/styled';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta: Meta<typeof Card> = {
   component: Card,
   subcomponents: {
+    'Card.Title': Card.Title,
     'Card.Description': Card.Description,
     'Card.Extra': Card.Extra,
-    'Card.Title': Card.Title,
+    'Card.Group': Card.Group,
   },
   tags: ['autodocs'],
-  parameters: { layout: 'centered' },
-  render: (e) => (
-    <Card {...e}>
-      <Card.Title>This is a title</Card.Title>
-      <Card.Description>This is the Description of your card </Card.Description>
-      <Card.Extra>This is a litle extra</Card.Extra>
-    </Card>
-  ),
+  argTypes: {
+    space: {
+      control: 'select',
+      options: ['xxs', 'xs', 'sm', 'md', 'lg', 'xl', 'xxl'],
+    },
+    center: { control: 'boolean' },
+  },
 };
 
 export default meta;
@@ -33,9 +38,62 @@ type Story = StoryObj<typeof Card>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
-  parameters: {
-    docs: {
-      source: { language: 'tsx' },
-    },
+  args: {
+    center: false,
   },
+  render: (e) => (
+    <Card {...e}>
+      <Card.Title>This is a Title</Card.Title>
+      <Card.Description>This is a Description for your card</Card.Description>
+      <Card.Extra>This is a litle Extra</Card.Extra>
+    </Card>
+  ),
+};
+
+export const OtherCard: Story = {
+  ...Primary,
+  render: (e) => (
+    <Card {...e}>
+      <XBox justifyContent="between" space="sm">
+        <Text>Label</Text>
+        <Text>Value</Text>
+      </XBox>
+    </Card>
+  ),
+};
+
+export const OneElement: Story = {
+  ...Primary,
+  render: (e) => (
+    <Card.Group>
+      {Primary.render(e, undefined)}
+      <Divider />
+      {OtherCard.render(e, undefined)}
+      <Divider />
+      {OtherCard.render(e, undefined)}
+    </Card.Group>
+  ),
+};
+
+export const CardStat: Story = {
+  ...Primary,
+  render: (e) => (
+    <Box
+      style={inlineStyle(() => ({
+        base: { width: '100%' },
+        media: { md: { width: '25%' } },
+      }))}
+    >
+      <Card.Group>
+        <Card {...e}>
+          <Card.Title textAlign="center">00,00$</Card.Title>
+          <Card.Description textAlign="center">Obtenu</Card.Description>
+        </Card>
+        <Divider />
+        {OtherCard.render(e, undefined)}
+        <Divider />
+        {OtherCard.render(e, undefined)}
+      </Card.Group>
+    </Box>
+  ),
 };
---
+++ packages/ui/src/display/__tests__/Card.spec.tsx
@@ -0,0 +1,172 @@
+/**
+ * Copyright (c) Paymium.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root of this projects source tree.
+ */
+
+import { render, screen } from '@crossed/test';
+import '@testing-library/jest-dom';
+import { Text } from '../../typography';
+import { Divider } from '../../layout';
+import {
+  Card,
+  CardTitle,
+  CardDescription,
+  CardGroup,
+  CardExtra,
+} from '../Card';
+
+expect(Card).toBeDefined();
+expect(CardTitle).toBeDefined();
+expect(CardDescription).toBeDefined();
+expect(CardGroup).toBeDefined();
+expect(CardExtra).toBeDefined();
+
+describe('Card Group', () => {
+  test('if child not valid it is not rendered', () => {
+    render(
+      <Card.Group>
+        <Card>
+          <Card.Title>Toto</Card.Title>
+        </Card>
+        {42}
+      </Card.Group>
+    );
+    expect(screen.getByText('Toto')).toBeInTheDocument();
+    expect(screen.queryByText('42')).not.toBeInTheDocument();
+  });
+
+  test('Throw error with direct child different from Card or Divider', () => {
+    const renderWithInvalidChild = () =>
+      render(
+        <Card.Group>
+          <Text>Toto</Text>
+        </Card.Group>
+      );
+    expect(renderWithInvalidChild).toThrow(
+      'Direct children of CardGroup should be Divider or Card'
+    );
+  });
+
+  test('Divider is rendered ', () => {
+    render(
+      <Card.Group>
+        <Divider testID="divider" />
+      </Card.Group>
+    );
+    expect(screen.getByTestId('divider')).toBeTruthy();
+  });
+
+  test("With only one Child CardGroup doesn't change style of child", () => {
+    render(
+      <Card.Group>
+        <Card testID="firstCardChild">
+          <Card.Title>Toto</Card.Title>
+        </Card>
+      </Card.Group>
+    );
+
+    const element = screen.getByTestId('firstCardChild');
+    expect(element).not.toHaveClass(
+      'border-top-left-radius-[0px] border-bottom-left-radius-[0px] border-top-right-radius-[0px] border-bottom-right-radius-[0px]'
+    );
+  });
+
+  describe('With 2 Card Children', () => {
+    const customRender = () => {
+      render(
+        <Card.Group>
+          <Card testID="firstCardChild">
+            <Card.Title>Toto</Card.Title>
+          </Card>
+          <Card testID="lastCardChild">
+            <Card.Title>Toti</Card.Title>
+          </Card>
+        </Card.Group>
+      );
+    };
+    test('border bottom and border radius bottom of first child are modified', () => {
+      customRender();
+      const firstCardChild = screen.getByTestId('firstCardChild');
+      expect(firstCardChild).not.toHaveClass(
+        'border-top-left-radius-[0px] border-top-right-radius-[0px]'
+      );
+      expect(firstCardChild).toHaveClass(
+        'border-bottom-left-radius-[0px] border-bottom-right-radius-[0px] border-bottom-width-[0px]'
+      );
+    });
+
+    test('border top and border radius top of last child are modified', () => {
+      customRender();
+      const lastCardChild = screen.getByTestId('lastCardChild');
+      expect(lastCardChild).toHaveClass(
+        'border-top-left-radius-[0px] border-top-right-radius-[0px] border-top-width-[0px]'
+      );
+      expect(lastCardChild).not.toHaveClass(
+        'border-bottom-left-radius-[0px] border-bottom-right-radius-[0px]'
+      );
+    });
+  });
+
+  describe('With 3 Card Children', () => {
+    const customRender = () => {
+      render(
+        <Card.Group>
+          <Card testID="firstCardChild">
+            <Card.Title>Toto</Card.Title>
+          </Card>
+          <Card testID="middleCardChild">
+            <Card.Title>Titi</Card.Title>
+          </Card>
+          <Card testID="lastCardChild">
+            <Card.Title>Toti</Card.Title>
+          </Card>
+        </Card.Group>
+      );
+    };
+    test('border bottom and border radius bottom of first child are modified', () => {
+      customRender();
+      const firstCardChild = screen.getByTestId('firstCardChild');
+      expect(firstCardChild).not.toHaveClass(
+        'border-top-left-radius-[0px] border-top-right-radius-[0px]'
+      );
+      expect(firstCardChild).toHaveClass(
+        'border-bottom-left-radius-[0px] border-bottom-right-radius-[0px] border-bottom-width-[0px]'
+      );
+    });
+
+    test('border top and bottom and border radius top and bottom of middle child are modified', () => {
+      customRender();
+      const middleCardChild = screen.getByTestId('middleCardChild');
+      expect(middleCardChild).toHaveClass(
+        'border-radius-[0px] border-bottom-width-[0px] border-top-width-[0px]'
+      );
+    });
+
+    test('border top and border radius top of last child are modified', () => {
+      customRender();
+      const lastCardChild = screen.getByTestId('lastCardChild');
+      expect(lastCardChild).toHaveClass(
+        'border-top-left-radius-[0px] border-top-right-radius-[0px] border-top-width-[0px]'
+      );
+      expect(lastCardChild).not.toHaveClass(
+        'border-bottom-left-radius-[0px] border-bottom-right-radius-[0px]'
+      );
+    });
+  });
+});
+
+describe('Card', () => {
+  test('if role link, link style is applied', () => {
+    render(
+      <Card role="link" testID="card">
+        <Card.Title>Toto</Card.Title>
+      </Card>
+    );
+    expect(screen.getByTestId('card')).toHaveClass(
+      'active:background-color-[var(--components--card-active-background)]'
+    );
+    expect(screen.getByTestId('card')).toHaveAttribute('role', 'link');
+  });
+});
