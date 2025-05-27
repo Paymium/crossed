664716cc4f086062b42ca9e77cb2b232a5386739
aymerick <aymerick.batlogg@paymium.com>
2025-05-27 17:02:02 +0200

    add border Radius spacing preset

---
+++ .changeset/great-drinks-kneel.md
@@ -0,0 +1,5 @@
+---
+'@crossed/theme': patch
+---
+
+add borderRadius preset
---
+++ packages/theme/src/types/borderRadius.ts
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Paymium.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root of this projects source tree.
+ */
+
+export type borderRadiusName =
+  | 'none'
+  | 'xxs'
+  | 'xs'
+  | 'sm'
+  | 'md'
+  | 'lg'
+  | 'xl'
+  | '2xl'
+  | '3xl'
+  | '4xl'
+  | 'full';
+export type BorderRadius = Record<borderRadiusName, number>;
