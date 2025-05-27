8bdb24109f4b03d9c0262c3a14e3856ef17dee23
aymerick <aymerick.batlogg@paymium.com>
2025-05-27 16:51:31 +0200

    add primitive space and preset space

--- packages/theme/src/types/space.ts
+++ packages/theme/src/types/space.ts
@@ -7,3 +7,57 @@
 
 export type SpaceName = 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
 export type Spaces = Record<SpaceName, number>;
+
+export type PrimitiveSPaceName =
+  | 0
+  | 0.5
+  | 1
+  | 1.5
+  | 2
+  | 3
+  | 4
+  | 5
+  | 6
+  | 8
+  | 10
+  | 12
+  | 16
+  | 20
+  | 24
+  | 32
+  | 40
+  | 48
+  | 56
+  | 64
+  | 80
+  | 96
+  | 120
+  | 140
+  | 160
+  | 180
+  | 192
+  | 256
+  | 320
+  | 360
+  | 400
+  | 480;
+export type PresetSpaceName =
+  | 'none'
+  | 'xxs'
+  | 'xs'
+  | 'sm'
+  | 'md'
+  | 'lg'
+  | 'xl'
+  | '2xl'
+  | '3xl'
+  | '4xl'
+  | '5xl'
+  | '6xl'
+  | '7xl'
+  | '8xl'
+  | '9xl'
+  | '10xl'
+  | '11xl';
+export type PresetSpace = Record<PresetSpaceName, number>;
+export type PrimitiveSpace = Record<PrimitiveSPaceName, number>;
