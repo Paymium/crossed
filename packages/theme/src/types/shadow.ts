6f4d757c939d5f8c477045101f9e9cc1fbf0df6f
aymerick <aymerick.batlogg@paymium.com>
2025-05-27 17:42:22 +0200

    add shadow preset

---
+++ packages/theme/src/types/shadow.ts
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) Paymium.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root of this projects source tree.
+ */
+
+export type ShadowName = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl';
+export type Shadow = Record<ShadowName, string>;
