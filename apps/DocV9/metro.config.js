0e1d40053d9e119e6c5002ff16ede696fdb3e8a4
lobor <lionel.bertrand@paymium.com>
2025-06-12 09:27:43 +0200

    fix

--- apps/DocV9/metro.config.js
+++ apps/DocV9/metro.config.js
@@ -1,25 +1,40 @@
-const { getDefaultConfig } = require("expo/metro-config");
+const path = require('path');
+const withStorybook = require('@storybook/react-native/metro/withStorybook');
 
-const defaultConfig = getDefaultConfig(__dirname);
-
-// const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
-const withStorybook = require("@storybook/react-native/metro/withStorybook");
+const { makeMetroConfig } = require('@rnx-kit/metro-config');
 const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
+const { getDefaultConfig } = require('expo/metro-config');
+const {
+  CyclicDependencies,
+} = require('@rnx-kit/metro-plugin-cyclic-dependencies-detector');
+const { MetroSerializer } = require('@rnx-kit/metro-serializer');
+const {
+  DuplicateDependencies,
+} = require('@rnx-kit/metro-plugin-duplicates-checker');
 
-
-module.exports = withStorybook(
-  { ...defaultConfig,
-    resolver:
-      {
-        ...defaultConfig.resolver,
-        resolveRequest: MetroSymlinksResolver(),
-        unstable_enablePackageExports: true,
-        unstable_enableSymlinks: true,
-        enableGlobalPackages: false,
-      },
+let config = getDefaultConfig(__dirname);
+config = makeMetroConfig({
+  ...config,
+  serializer: {
+    ...config.serializer,
+    customSerializer: MetroSerializer([
+      DuplicateDependencies({
+        throwOnError: false,
+      }),
+      CyclicDependencies({
+        includeNodeModules: false,
+        linesOfContext: 1,
+        throwOnError: false,
+      }),
+    ]),
   },
-  {
-    enabled: process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === 'true',
-    onDisabledRemoveStorybook: true,
-  }
-);
+  resolver: {
+    ...config.resolver,
+    resolveRequest: MetroSymlinksResolver(),
+  },
+});
+
+module.exports = withStorybook(config, {
+  enabled: true,
+  configPath: path.resolve(__dirname, './.rnstorybook'),
+});
